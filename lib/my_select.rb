4947bf09d86f431da2c2383300b33a1de09b6568
Troy Gooden <tro12185@gmail.com>
2019-03-21 21:45:48 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  if block_given?
+    i = 0
+    selected = []
+
+    while i < array.length
+         if yield array[i] 
+            selected << array[i]
+         end
+         i += 1
+    end
+    selected
+  end
 end
